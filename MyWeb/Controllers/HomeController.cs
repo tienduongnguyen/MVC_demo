242f305d4e45b6dc5da57ee00ce690d4c483e407
Dương Nguyễn <ntduong1292@gmail.com>
2021-03-31 13:31:43 +0700

    add files

---
+++ MyWeb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult MenuLeft()
+        {
+            List<string> lst = new List<string>();
+            lst.Add("Quản lý 1");
+            lst.Add("Quản lý 2");
+            lst.Add("Quản lý 3");
+            return PartialView(lst);
+        }
+    }
+}
---
+++ MyWeb/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MyWeb
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Session_Start(object sender, EventArgs erg)
+        {
+            Application.Lock();
+            if (Application["Views"] == null)
+                Application["Views"] = 1;
+            else
+                Application["Views"] = (int)Application["Views"] + 1;
+            Application.UnLock();
+        }
+    }
+}
